e8979c5f0a67f16bea38a6804e23b4519c2d6eb5
Sebastian <Seb.Slowikowski@microchip.com>
2020-02-28 14:18:37 +0000

    Libero v12.3 design revamp
    This update includes a redesign of the TCL scripts.
    * Names of files, directories and components on the canvas.
    * Improved strcture of the TCL scripts (different component instantiation technique)
    * Updated Libero core components.

--- Libero_Projects/MPF300T-1FCG1152I.tcl
+++ Libero_Projects/MPF300T-1FCG1152I.tcl
@@ -1,18 +1,10 @@
-set project_folder_name_axi MiV_RV32IMA_L1_AXI_BaseDesign
+set project_folder_name_axi MiV_AXI_PF_Eval
 set project_dir_axi "./$project_folder_name_axi"
 set Libero_project_name_axi MiV_AXI_BaseDesign
 
-append Libero_project_name_axi "_MPF300T-1FCG1152I"
-append project_dir_axi "_MPF300T-1FCG1152I"
-append project_folder_name_axi "_MPF300T-1FCG1152I"
-
-set project_folder_name_ahb MiV_RV32IMA_L1_AHB_BaseDesign
+set project_folder_name_ahb MiV_AHB_PF_Eval
 set project_dir_ahb "./$project_folder_name_ahb"
 set Libero_project_name_ahb MiV_AHB_BaseDesign
-
-append Libero_project_name_ahb "_MPF300T-1FCG1152I"
-append project_dir_ahb "_MPF300T-1FCG1152I"
-append project_folder_name_ahb "_MPF300T-1FCG1152I"
 
 set target [string toupper [lindex $argv 0]]
 set design_flow_stage [string toupper [lindex $argv 1]]
@@ -23,7 +15,7 @@ proc create_new_project_label_axi { }\
 	puts "-------------------------------------------------------------------------"
     puts "-------------------------------------------------------------------------"
     puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "--------MiV_RV32IMA_L1_AXI_BaseDesign_MPF300T_EVAL_KIT-------------------"
+	puts "-------------MiV_RV32IMA_L1_AXI_BaseDesign_PF_EVAL_KIT-------------------"
     puts "-------------------------------------------------------------------------"
 	puts "-------------------------------------------------------------------------"
 }
@@ -33,7 +25,7 @@ proc create_new_project_label_ahb { }\
 	puts "-------------------------------------------------------------------------"
     puts "-------------------------------------------------------------------------"
     puts "-----------------------CREATING NEW PROJECT------------------------------"
-	puts "--------MiV_RV32IMA_L1_AHB_BaseDesign_MPF300T_EVAL_KIT-------------------"
+	puts "-------------MiV_RV32IMA_L1_AHB_BaseDesign_PF_EVAL_KIT-------------------"
     puts "-------------------------------------------------------------------------"
 	puts "-------------------------------------------------------------------------"
 }
@@ -47,13 +39,23 @@ proc project_exists { }\
 	puts "---------------------------------------------------------------------------"
 }
 
+proc invalid_argument { }\
+{
+	puts "---------------------------------------------------------------------------"
+    puts "----------------------WRONG ARGUMENT ENTERED !-----------------------------"
+	puts "---------------------------------------------------------------------------"
+    puts "Make sure you put AHB or AXI argument in front of the Design Flow argument!"
+	puts "---------------------------------------------------------------------------"
+}
+
+
 if {"$target" == "AXI"} then {
 	if {[file exists $project_dir_axi] == 1} then { 
 		project_exists
 	} else { 
 		create_new_project_label_axi
 		new_project -location $project_dir_axi -name $Libero_project_name_axi -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:IND} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
-		source ./import/components/MPF300T-1FCG1152I/AXI/import_component_and_constraints_mpf300_eval_kit_axi.tcl
+		source ./import/components/PF_Eval/AXI/import_component_and_constraints_pf_eval_axi.tcl
 		save_project
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
@@ -67,7 +69,7 @@ if {"$target" == "AXI"} then {
 	} else {
 		create_new_project_label_ahb
 		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:IND} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
-		source ./import/components/MPF300T-1FCG1152I/AHB/import_component_and_constraints_mpf300_eval_kit_ahb.tcl
+		source ./import/components/PF_Eval/AHB/import_component_and_constraints_pf_eval_ahb.tcl
 		save_project
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
@@ -75,71 +77,104 @@ if {"$target" == "AXI"} then {
 		puts "----------------------------BaseDesign Built-----------------------------"
 		puts "-------------------------------------------------------------------------"
 	}
+} elseif {"$target" == "SYNTHESIZE"} then {
+		invalid_argument
+} elseif {"$target" == "PLACE_AND_ROUTE"} then {
+		invalid_argument
+} elseif {"$target" == "GENERATE_BITSTREAM"} then {
+		invalid_argument
+} elseif {"$target" == "EXPORT_PROGRAMMING_FILE"} then {
+		invalid_argument
 } else {
 	if {[file exists $project_dir_ahb] == 1} then {
 		project_exists
 	} else {
 		create_new_project_label_ahb
 		new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {SYSTEM_CONTROLLER_SUSPEND_MODE:0} -adv_options {TEMPR:IND} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
-		source ./import/components/MPF300T-1FCG1152I/AHB/import_component_and_constraints_mpf300_eval_kit_ahb.tcl
+		source ./import/components/PF_Eval/AHB/import_component_and_constraints_pf_eval_ahb.tcl
 		save_project
+		puts " "
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------SUCCESS!----------------------------------"
 		puts "-------------------------------------------------------------------------"
 		puts "----------------------------BaseDesign Built-----------------------------"
 		puts "-------------------------------------------------------------------------"
+		puts " "
+		puts " "
 	}
 }
 
-# Configuring Place_and_Route tool for a timing pass.
-configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
-# ##
 
 if {"$design_flow_stage" == "SYNTHESIZE"} then {
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "-----------------------------SYNTHESIS-----------------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
+	# Configuring Place_and_Route tool for a timing pass.
+	configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
+	# ##
     run_tool -name {SYNTHESIZE}
     save_project
 
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "---------------------------SYNTHESIZED!----------------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 	
 } elseif {"$design_flow_stage" == "PLACE_AND_ROUTE"} then {
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------------PLACE AND ROUTE--------------------------------"
     puts "-------------------------------------------------------------------------"
-	
+	puts " "
+
+	# Configuring Place_and_Route tool for a timing pass.
+	configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
+	# ##
 	run_tool -name {PLACEROUTE}
-	run_tool -name {VERIFYTIMING}
 	save_project
 	
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "----------------------DESIGN PLACED AND ROUTED!--------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
 	
 } elseif {"$design_flow_stage" == "GENERATE_BITSTREAM"} then {
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------GENERATING PROGRAMMING FILES-------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
+	# Configuring Place_and_Route tool for a timing pass.
+	configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
+	# ##
     run_tool -name {GENERATEPROGRAMMINGDATA}
     run_tool -name {GENERATEPROGRAMMINGFILE}
     save_project
 	
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------PROGRAMMING FILES GENERATED!-------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
 
 } elseif {"$design_flow_stage" == "EXPORT_PROGRAMMING_FILE"} then {
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "----------------------EXPORT PROGRAMMING FILES---------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
+# Configuring Place_and_Route tool for a timing pass.
+configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
+# ##
 # pre-requisite to enable exporting programming file function
     run_tool -name {GENERATEPROGRAMMINGDATA}
     run_tool -name {GENERATEPROGRAMMINGFILE}
@@ -147,7 +182,7 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
 
 	if {"$target" == "AHB"} then {
 		export_prog_job \
-			-job_file_name {BaseDesign_MPF300T-AHB-ProgramFile} \
+			-job_file_name {BaseDesign_PF_Eval_AHB_ProgramFile} \
 			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {}
@@ -155,33 +190,89 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
 		
 	} else {
 		export_prog_job \
-			-job_file_name {BaseDesign_MPF300T-AXI-ProgramFile} \
+			-job_file_name {BaseDesign_PF_Eval_AXI_ProgramFile} \
 			-export_dir {./../FlashPro_Express_Projects/Programming_Files} \
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {}
 		save_project
 	}
 	
+	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------PROGRAMMING FILES EXPORTED!--------------------------"
     puts "-------------------------------------------------------------------------"
+	puts " "
 
 } else {
-    puts "-------------------------------------------------------------------------"
-	puts "-------------------------------------------------------------------------"
-	puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
-	puts "--------------------------(this is optional)-----------------------------"
-	puts "-------------------------------------------------------------------------"
+
+	if {"$target" == "AHB"} then {
+
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
+		puts "--------------------------(this is optional)-----------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------SCRIPT EXECUTED!--------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+
+	
+	} elseif {"$target" == "AXI"} then {
+	
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
+		puts "--------------------------(this is optional)-----------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------SCRIPT EXECUTED!--------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+	
+	} else {
+		puts " "
+		puts " "
+		puts " "
+		puts " "
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-----------------NO VALID DESIGN FLOW ARGUMENT ENTERED!------------------"
+		puts "--------------------------(this is optional)-----------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------ENTER A VALID FIRST ARGUMENT TO USE DESIGN FLOW ARGUMENT----------"
+		puts "------------------------------AHB OR AXI---------------------------------"
+		puts "-------------------------------------------------------------------------"
+		puts "-------------------------------------------------------------------------"
+
+	}
 }
-
-
-puts ""
-puts ""
-puts ""
-puts ""
-puts "-------------------------------------------------------------------------"
-puts "-------------------------------------------------------------------------"
-puts "----------------------SCRIPT EXECUTED SUCCESSFULLY!----------------------"
-puts "-------------------------------------------------------------------------"
-puts "-------------------------------------------------------------------------"
 
