9cf716b51251b7efe9564be1ba41ccbebf1cbfdb
Ciaran Lappin <Ciaran.Lappin@microchip.com>
2020-03-06 17:01:35 +0000

    Update Libero folder and project names.

--- Libero_Projects/MPF300T_ES-1FCG1152I.tcl
+++ Libero_Projects/MPF300T_ES-1FCG1152I.tcl
@@ -1,10 +1,10 @@
-set project_folder_name_axi MiV_AXI_PF_Eval_ES
+set project_folder_name_axi MiV_AXI_BD
 set project_dir_axi "./$project_folder_name_axi"
-set Libero_project_name_axi MiV_AXI_BaseDesign
+set Libero_project_name_axi PF_EVAL_KIT_ES_MiV_AXI_BaseDesign
 
-set project_folder_name_ahb MiV_AHB_PF_Eval_ES
+set project_folder_name_ahb MiV_AHB_BD
 set project_dir_ahb "./$project_folder_name_ahb"
-set Libero_project_name_ahb MiV_AHB_BaseDesign
+set Libero_project_name_ahb PF_EVAL_KIT_ES_MiV_AHB_BaseDesign
 
 set target [string toupper [lindex $argv 0]]
 set design_flow_stage [string toupper [lindex $argv 1]]
@@ -50,11 +50,11 @@ proc invalid_argument { }\
 
 
 if {"$target" == "AXI"} then {
-	if {[file exists $project_dir_axi] == 1} then { 
+	if {[file exists $project_dir_axi] == 1} then {
 		project_exists
-	} else { 
+	} else {
 		create_new_project_label_axi
-	    new_project -location $project_dir_axi -name $Libero_project_name_axi -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
+	    new_project -location $project_dir_axi -name $Libero_project_name_axi -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND}
 		source ./import/components/PF_Eval_ES/AXI/import_component_and_constraints_pf_eval_es_axi.tcl
 		save_project
 		puts "-------------------------------------------------------------------------"
@@ -68,7 +68,7 @@ if {"$target" == "AXI"} then {
 		project_exists
 	} else {
 		create_new_project_label_ahb
-     	new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
+     	new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND}
 		source ./import/components/PF_Eval_ES/AHB/import_component_and_constraints_pf_eval_es_ahb.tcl
 		save_project
 		puts "-------------------------------------------------------------------------"
@@ -90,7 +90,7 @@ if {"$target" == "AXI"} then {
 		project_exists
 	} else {
 		create_new_project_label_ahb
-     	new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND} 
+     	new_project -location $project_dir_ahb -name $Libero_project_name_ahb -project_description {} -block_mode 0 -standalone_peripheral_initialization 0 -instantiate_in_smartdesign 1 -ondemand_build_dh 1 -hdl {VERILOG} -family {PolarFire} -die {MPF300T_ES} -package {FCG1152} -speed {-1} -die_voltage {1.0} -part_range {IND} -adv_options {IO_DEFT_STD:LVCMOS 1.8V} -adv_options {RESTRICTPROBEPINS:1} -adv_options {RESTRICTSPIPINS:0} -adv_options {TEMPR:IND} -adv_options {UNUSED_MSS_IO_RESISTOR_PULL:None} -adv_options {VCCI_1.2_VOLTR:EXT} -adv_options {VCCI_1.5_VOLTR:EXT} -adv_options {VCCI_1.8_VOLTR:EXT} -adv_options {VCCI_2.5_VOLTR:EXT} -adv_options {VCCI_3.3_VOLTR:EXT} -adv_options {VOLTR:IND}
 		source ./import/components/PF_Eval_ES/AHB/import_component_and_constraints_pf_eval_es_ahb.tcl
 		save_project
 		puts " "
@@ -123,27 +123,27 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
     puts "---------------------------SYNTHESIZED!----------------------------------"
     puts "-------------------------------------------------------------------------"
 	puts " "
-	
+
 } elseif {"$design_flow_stage" == "PLACE_AND_ROUTE"} then {
 	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------------PLACE AND ROUTE--------------------------------"
     puts "-------------------------------------------------------------------------"
 	puts " "
-	
+
 	# Configuring Place_and_Route tool for a timing pass.
 	configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MIN_DELAY:true} -params {TDPR:true}
 	# ##
 	run_tool -name {PLACEROUTE}
 	save_project
-	
+
 	puts " "
     puts "-------------------------------------------------------------------------"
     puts "----------------------DESIGN PLACED AND ROUTED!--------------------------"
     puts "-------------------------------------------------------------------------"
 	puts " "
 
-	
+
 } elseif {"$design_flow_stage" == "GENERATE_BITSTREAM"} then {
 	puts " "
     puts "-------------------------------------------------------------------------"
@@ -157,7 +157,7 @@ if {"$design_flow_stage" == "SYNTHESIZE"} then {
     run_tool -name {GENERATEPROGRAMMINGDATA}
     run_tool -name {GENERATEPROGRAMMINGFILE}
     save_project
-	
+
 	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------PROGRAMMING FILES GENERATED!-------------------------"
@@ -187,7 +187,7 @@ configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MI
 			-bitstream_file_type {TRUSTED_FACILITY} \
 			-bitstream_file_components {}
 		save_project
-		
+
 	} else {
 		export_prog_job \
 			-job_file_name {BaseDesign_PF_Eval_ES_AXI_ProgramFile} \
@@ -196,7 +196,7 @@ configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MI
 			-bitstream_file_components {}
 		save_project
 	}
-	
+
 	puts " "
     puts "-------------------------------------------------------------------------"
     puts "--------------------PROGRAMMING FILES EXPORTED!--------------------------"
@@ -231,9 +231,9 @@ configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MI
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------------------------------------------------"
 
-	
+
 	} elseif {"$target" == "AXI"} then {
-	
+
 		puts " "
 		puts " "
 		puts " "
@@ -257,7 +257,7 @@ configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MI
 		puts "-------------------------SCRIPT EXECUTED!--------------------------------"
 		puts "-------------------------------------------------------------------------"
 		puts "-------------------------------------------------------------------------"
-	
+
 	} else {
 		puts " "
 		puts " "
@@ -275,4 +275,3 @@ configure_tool -name {PLACEROUTE} -params {EFFORT_LEVEL:true} -params {REPAIR_MI
 
 	}
 }
-
